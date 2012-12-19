e141436fd61186c60570e44cb08ef9d1e1878995
El.Pnjito <El.Pnjito@gmail.com>
2012-12-19 22:42:16 +0000

    Modif ViewAllparties

--- MarioWare_web/src/view/PartyCreation.java
+++ MarioWare_web/src/view/PartyCreation.java
@@ -2,6 +2,7 @@ package view;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PartyCreation
  */
+
+
 public class PartyCreation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
