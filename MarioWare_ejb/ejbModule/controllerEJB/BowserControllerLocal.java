cbf7552961019bffe301d13c6808f61c47918ed1
clement.kawczak <clement.kawczak@gmail.com>
2012-12-20 05:50:07 +0000

    Modification structure , maintenant opérationnel

--- MarioWare_ejb/ejbModule/controllerEJB/BowserControllerLocal.java
+++ MarioWare_ejb/ejbModule/controllerEJB/BowserControllerLocal.java
@@ -8,5 +8,7 @@ public interface BowserControllerLocal {
 	public void setValeur(int id);
 	public int getValeur();
 	public void addParty(int valeur,String JNDI_NAME);
-	public String affiche(); 
+	public String affiche();
+	public int getValeurGame(int indexParty,int indexGame);
+	public void setValeurGame(int indexParty,int indexGame, int valeur);
 }
