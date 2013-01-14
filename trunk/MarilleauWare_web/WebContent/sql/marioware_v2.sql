58516410de29719e225baaa963b69632832203f5
pierremartin799@gmail.com <pierremartin799@gmail.com@523d572e-780b-ebde-f4e8-6133ff7c8053>
2013-01-14 22:57:42 +0000

    Mise à jour de la BDD

--- trunk/MarilleauWare_web/WebContent/sql/marioware_v2.sql
+++ trunk/MarilleauWare_web/WebContent/sql/marioware_v2.sql
@@ -1,14 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Genere le : Ven 14 Decembre 2012 a 14:32
--- Version du serveur: 5.5.16
--- Version de PHP: 5.3.8
+-- Généré le: Lun 14 Janvier 2013 à 22:56
+-- Version du serveur: 5.5.16-log
+-- Version de PHP: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,8 +17,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de donnees: `marioware`
+-- Base de données: `marioware2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `idGame` int(11) NOT NULL AUTO_INCREMENT,
+  `idGame_desc` int(11) NOT NULL,
+  `idParty` int(11) NOT NULL,
+  `startGame` int(1) NOT NULL,
+  `endGame` int(1) NOT NULL,
+  `sequence` int(11) NOT NULL,
+  PRIMARY KEY (`idGame`),
+  KEY `idGame_desc` (`idGame_desc`),
+  KEY `idParty` (`idParty`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=226 ;
 
 -- --------------------------------------------------------
 
@@ -30,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `games_desc` (
   `name` varchar(64) NOT NULL,
   `description` varchar(128) NOT NULL,
   PRIMARY KEY (`idGame_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -46,44 +65,10 @@ CREATE TABLE IF NOT EXISTS `parties` (
   `startParty` int(1) NOT NULL,
   `endParty` int(1) NOT NULL,
   `idCurrentGame` int(11) NOT NULL,
-  PRIMARY KEY (`idParty`),
-  FOREIGN KEY(idUserCreate) references users(idUser)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`idParty`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
 
 -- --------------------------------------------------------
-
---
--- Structure de la table `games`
---
-
-CREATE TABLE IF NOT EXISTS `games` (
-  `idGame` int(11) NOT NULL AUTO_INCREMENT,
-  `idGame_desc` int(11) NOT NULL,
-  `idParty` int(11) NOT NULL,
-  `startGame` int(1) NOT NULL,
-  `endGame` int(1) NOT NULL,
-  PRIMARY KEY (`idGame`),
-  FOREIGN KEY(idGame_desc) references games_desc(idGame_desc),
-  FOREIGN KEY(idParty) references parties(idParty)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(32) NOT NULL,
-  `password` varchar(64),
-  `isAdmin` int(11) NOT NULL,
-  `score` int(11) NOT NULL,
-  `idParty` int(11),
-  PRIMARY KEY (`idUser`),
-  FOREIGN KEY(idParty) references parties(idParty)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Structure de la table `tj_games_users`
@@ -94,10 +79,49 @@ CREATE TABLE IF NOT EXISTS `tj_games_users` (
   `idUser` int(11) NOT NULL,
   `score` int(11) NOT NULL,
   PRIMARY KEY (`idGame`,`idUser`),
-  FOREIGN KEY(idGame) references games(idGame),
-  FOREIGN KEY(idUser) references users(idUser)
+  KEY `idUser` (`idUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(32) NOT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `isAdmin` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `idParty` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idUser`),
+  KEY `idParty` (`idParty`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `games`
+--
+ALTER TABLE `games`
+  ADD CONSTRAINT `games_ibfk_1` FOREIGN KEY (`idGame_desc`) REFERENCES `games_desc` (`idGame_desc`),
+  ADD CONSTRAINT `games_ibfk_2` FOREIGN KEY (`idParty`) REFERENCES `parties` (`idParty`);
+
+--
+-- Contraintes pour la table `tj_games_users`
+--
+ALTER TABLE `tj_games_users`
+  ADD CONSTRAINT `tj_games_users_ibfk_1` FOREIGN KEY (`idGame`) REFERENCES `games` (`idGame`),
+  ADD CONSTRAINT `tj_games_users_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`);
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idParty`) REFERENCES `parties` (`idParty`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
