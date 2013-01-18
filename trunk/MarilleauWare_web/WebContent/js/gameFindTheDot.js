00977443818fa016b57a115354083a44d19052fb
El.Pnjito@gmail.com <El.Pnjito@gmail.com@523d572e-780b-ebde-f4e8-6133ff7c8053>
2013-01-18 17:24:47 +0000

    Changement game1.js en FindTheDot sur tous les fichiers correspondant

---
+++ trunk/MarilleauWare_web/WebContent/js/gameFindTheDot.js
@@ -0,0 +1,204 @@
+
+var xhr;
+
+/* Context graphique */
+var context;
+var hasplayed = false;
+var idCount;
+var idRefresh;
+var idEnd;
+var d;
+var end;
+var s, ms;
+var idUser;
+var idParty;
+var idGame;
+
+function salut(){
+	alert("ok");
+}
+
+function begin1(x,y){
+	alert("coucou");
+}
+
+function runFindTheDot(idUserServ, idPartyServ, idGameServ, goalX ,goalY, xInverted, yInverted){
+	idUser = idUserServ;
+	idParty = idPartyServ;
+	idGame = idGameServ;
+	var elem = document.getElementById('canvasElem');
+	if (!elem || !elem.getContext) {
+		return;
+	}
+	context = elem.getContext('2d');
+	if (!context) {
+		return;
+	}
+	context.clearRect(0,0,elem.width,elem.height);
+	document.getElementById('headConteneur').innerHTML += "Click on the (" + goalX + "," + goalY + ") pixel !";
+
+	// Affichage de l'échelle
+	for(i = 50;i <= 250;i += 50)
+	{
+		context.fillRect(0, i+1, 5, 2);
+		if(yInverted == 1)
+		{	
+			goalY = 300 - goalY;
+			context.fillText(300-i, 7, i+5);
+		}
+		else
+		{
+			context.fillText(i, 7, i+5);
+		}
+	}
+	
+	for(j = 50;j <= 350;j += 50)
+	{
+		context.fillRect(j-1, 0, 2, 5);
+		if(xInverted == 1)
+		{
+			goalX = 400 - goalX;
+			context.fillText(400-j, j-5, 15);
+		}
+		else
+		{
+			context.fillText(j, j-5, 15);
+		}
+	}
+	
+	hasplayed = false;
+	d = new Date();
+	idCount = setInterval(count, 10);
+
+	var conteneur = document.getElementById('bc_games');
+	
+	conteneur.onmousemove = function(event){
+		var left = 0;
+		var top = 0;
+		/*On récupère l'élément*/
+		var e = document.getElementById('bc_games');
+		/*Tant que l'on a un élément parent*/
+		while (e.offsetParent != undefined && e.offsetParent != null)
+		{
+			/*On ajoute la position de l'élément parent*/
+			left += e.offsetLeft + (e.clientLeft != null ? e.clientLeft : 0);
+			top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+			e = e.offsetParent;
+		}
+		var x = event.clientX;
+		var y = event.clientY;
+		var divInfo = document.getElementById('infos');
+		x = x - left;
+	};
+	// recuperer click souris
+	conteneur.onclick = function(event){
+		
+	if(!hasplayed)
+	{
+		var left = 0;
+		var top = 0;
+		var e = document.getElementById('bc_games');
+		while (e.offsetParent != undefined && e.offsetParent != null)
+		{
+			/*On ajoute la position de l'élément parent*/
+			left += e.offsetLeft + (e.clientLeft != null ? e.clientLeft : 0);
+			top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+			e = e.offsetParent;
+		}
+		var x = event.clientX;
+		var y = event.clientY;
+		x = x - left;
+		y = y - top;
+		
+		context.beginPath();
+		context.moveTo(x,y);
+		context.lineTo(goalX,goalY);
+		context.stroke();
+		
+		var d = Math.sqrt((x-goalX) * (x-goalX) + (y-goalY) * (y-goalY));
+		var distance = Math.floor(d);
+
+		var distanceX = goalX - (goalX - x)/2;
+		var distanceY = goalY - (goalY - y)/2;
+		context.fillText("distance = " + distance, distanceX, distanceY);
+	
+		context.fillStyle="#EE0000";
+		context.fillRect(goalX-1, goalY-1, 3, 3);
+		
+		context.fillStyle="#000000";
+		context.fillRect(x-1, y-1, 3, 3);
+		hasplayed = true;
+		clearInterval(idCount);
+		clearInterval(idRefresh);
+		finish(distance);
+		}
+	};
+}
+
+function finish(distance){
+	var servletName = "FindTheDot";
+	xhr = getXhr();
+	xhr.open("POST", "./" + servletName, true);
+	xhr.onreadystatechange = function(){
+	    if(xhr.readyState == 4 && xhr.status == 200){
+	        response = xhr.responseText;
+	    	document.getElementById('infos').innerHTML = response;
+	   }
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	document.getElementById('infos').innerHTML += "<br/><br/>action=2&idUser=" + idUser + "&idParty="+ idParty + "&idGame=" + idGame+ "&distance=" + distance;
+	xhr.send("action=2&idUser=" + idUser + "&idParty="+ idParty + "&idGame=" + idGame+ "&distance=" + distance);
+	idEnd = setInterval(checkResult(), 500);
+}
+
+
+
+function count(){
+	end = new Date();
+	var delta = end - d;
+	delta = new Date(delta);
+	ms = delta.getMilliseconds();
+	ms2 = 1000 - ms;
+	s = delta.getSeconds();
+	s2 = 30-s;
+	if(s2 <= -1)
+	{
+		s2 = 0;
+		ms2 = 0 + "00";
+		finish(999);
+		clearInterval(idCount);
+		clearInterval(idRefresh);
+		hasplayed = true;
+	}
+	var chrono = document.getElementById('chrono');
+	chrono.innerHTML = "Time : " + s2 + ":" + ms2;
+}
+
+function checkResult(){
+	
+	var servletName = "FindTheDot";
+	var tabId = [];
+	xhr.open("POST", "./" + servletName, true);
+	xhr.onreadystatechange = function(){
+	    if(xhr.readyState == 4 && xhr.status == 200){
+	        response = xhr.responseText;
+	    	document.getElementById('infos').innerHTML += "<br>" + response;
+	    	/* end_numJeuSuivant;param1;param2;param3;... */
+	    	if(response.substring(0,4)=="end_"){
+		    	clearInterval(idEnd);
+	    		var nextGame = response.substring(4,5);
+		    	tabId = response.substring(5,response.length).split(";");
+		    	document.getElementById('infos').innerHTML += "<br>idPlayer =>  " + tabId[0] + "  idParty => " + tabId[1] + " idGame => " + tabId[2];
+	    		
+		    	document.getElementById('infos').innerHTML += "<br>Avant initCheck " + response.substring(0,4);
+		    	
+		    	//Appel du jeu
+		    	runFindTheDot(tabId[0], tabId[1], tabId[2], tabId[3] ,tabId[4], tabId[5], tabId[6]);
+		    	
+		    	document.getElementById('infos').innerHTML += "<br>Apres InitCheck";
+	    	}
+	   }
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("action=3");	
+}
