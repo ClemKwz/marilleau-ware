e645e63bf444c1763c4ba4608eee6a82955f32be
pierremartin799@gmail.com <pierremartin799@gmail.com@523d572e-780b-ebde-f4e8-6133ff7c8053>
2013-01-23 17:44:54 +0000

    Ajout de fonction pour la récupération des scores des parties

---
+++ trunk/MarilleauWare_web/src/linkParty/ScoreParty.java
@@ -0,0 +1,66 @@
+package linkParty;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.TreeMap;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controllerEJB.PartyController2Local;
+
+/**
+ * Servlet implementation class ScoreParty
+ */
+public class ScoreParty extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@EJB
+	PartyController2Local pc;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ScoreParty() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		int idParty = Integer.parseInt(request.getParameter("idParty"));
+		
+		TreeMap<String, Integer> listscore = pc.getAllScore(idParty);
+		
+		String recupScore;
+		int i =1;
+		recupScore = "<table><tr><td>Rank</td><td>Pseudo</td><td>Score</td> ";
+		for(String login : listscore.keySet()){
+			recupScore = recupScore + "<tr>" +
+								"<td>" + i + ". </td>" +
+								"<td>" + login +" : </td>" +
+								"<td>" + listscore.get(login) + "</td><tr>";
+			i++;
+		}
+		
+		recupScore = recupScore + "</table>";
+		out.print(recupScore);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
