2da139f4c9e5a65f0670054feee47b384044373b
El.Pnjito@gmail.com <El.Pnjito@gmail.com@523d572e-780b-ebde-f4e8-6133ff7c8053>
2013-01-24 06:09:49 +0000

    aze

--- trunk/MarilleauWare_ejb/ejbModule/managerDB/GameManagerLocal.java
+++ trunk/MarilleauWare_ejb/ejbModule/managerDB/GameManagerLocal.java
@@ -16,7 +16,7 @@ public interface GameManagerLocal {
 	public List<GamesDesc> getAllGamesDesc();
 	public void addUserGame(int idGame, int idUser);
 	public int getIdGameByPartyBySeq(int idParty, int sequence);
-	public void addScore(int idGame, int idUser, double score);
+	public void addScore(int idGame, int idUser, int score);
 	public boolean containsNegativeScore(int idGame);
 	public int getNextgame(int idParty);
 	public HashMap<String, Integer> getAllScore(int idGame);
