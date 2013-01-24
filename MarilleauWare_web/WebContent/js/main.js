c5045411262edcd859d3d48ff53906b5e0e5cd6c
pierremartin799 <PierreMartin799@gmail.com>
2013-01-24 00:10:01 +0000

    mr propre

--- MarilleauWare_web/WebContent/js/main.js
+++ MarilleauWare_web/WebContent/js/main.js
@@ -1,15 +1,11 @@
 
+/** Variables **/
+
 var xhr;
 var idReloadGame;
 var idParty;
-//Initalisation d'un la première game
-//TODO:enlever le code en dur
-function init(id_deParty){
-	
-	idParty = id_deParty;
-	changeGame();
-	
-}
+
+/** Fonctions pour afficher et joindre les parties **/
 
 function initJoinPartie(idUser){
 	
@@ -25,110 +21,7 @@ function initJoinPartie(idUser){
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.send("action=1&idUser=" + idUser);
 }
-function initChat(idParty){
-	var v = setInterval("getAllMessage("+idParty+")", 100);
-}
-function getAllMessage(idParty){
-	var servletName = "Chat";
-	xhr = getXhr();
-	xhr.open("POST", "./" + servletName, true);
-	xhr.onreadystatechange = function(){
-		
-        if(xhr.readyState == 4 && xhr.status == 200) {
-            response = xhr.responseText;
-            document.getElementById('chat').innerHTML = response;
-         
-       }
-	};
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send("action=1&idParty="+idParty);
-}
 
-function sendMessage(idParty,idUser){
-	var servletName = "Chat";
-	xhr = getXhr();
-	xhr.open("POST", "./" + servletName, true);
-	xhr.onreadystatechange = function(){
-		
-        if(xhr.readyState == 4 && xhr.status == 200) {
-            response = xhr.responseText;
-            document.getElementById('chat').innerHTML = response;
-         
-       }
-	};
-	var msg = document.getElementsByName('message')[0].value;
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send("action=2&idParty="+idParty+"&idUser="+idUser+"&message="+msg);
-}
-//cette fonction permet de vérifier que le jeu courant est terminé
-//SI c'est le cas contact LinkParty pour lancer le nouveau jeu
-function reloadGame(){
-	//alert("je suis reload le casse couille");
-	var finished = document.getElementById('finishGame').value;
-
-	if(finished == "true"){
-
-		xhr = getXhr();
-		clearInterval(idReloadGame);
-		document.getElementById('finishGame').value = "false";
-		changeGame();
-		
-		
-		
-		
-	}
-}
-
-function changeGame(){
-	//récupération du nom du jeu suivant + idgamesuivan
-	var tabId = [];
-	xhr.open("POST", "./LinkParty", true);
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4 && xhr.status == 200){
-	        response = xhr.responseText;
-			
-			var servletNextGame = response
-			alert("ResponseLinkParty : "+ response);
-			//La partie est fini
-			if(response =="0"){
-				alert(fini);
-				return;
-			}
-			servletNextGame="FindTheDot";
-			
-			//on demande à la servlet concerncé de nous envoyé les paramètres
-			servletName = servletNextGame;
-			xhr = getXhr();
-			
-			xhr.open("POST", "./" + servletName, true);
-			xhr.onreadystatechange = function(){
-				
-		        if(xhr.readyState == 4 && xhr.status == 200) {
-		            response = xhr.responseText;
-		            document.getElementById('infos').innerHTML = response;
-		            var param = response.split(';');
-		            var stringParam="";
-		            
-		          //On créer la liste des paramètre sous forme de string
-					for(var i=0;i<param.length -1;i++){
-						stringParam += param[i]+","
-					}
-					stringParam += param[param.length-1];
-					
-					//On lance la fonction concerné
-		            eval("run"+servletNextGame+"(" + stringParam + ")");
-		            idReloadGame = setInterval("reloadGame()", 800);
-		       }
-			var info = document.getElementById("infos");
-			};
-			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-			xhr.send("action=1");
-		}
-		
-	};
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send("idParty="+idParty);
-}
 function joinPartie(idParty,idUser){
 	
 	var servletName = "JoinParty";
@@ -159,4 +52,151 @@ function joinPartie(idParty,idUser){
     };
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.send("action=2&idUser=" + idUser + "&idParty=" + idParty);
+}
+
+
+
+
+
+/** Fonctions pour le chargement et changement des games **/
+
+//Initalisation d'un la première game
+//TODO:enlever le code en dur
+function init(id_deParty){
+	
+	idParty = id_deParty;
+	changeGame();
+	
+}
+
+//cette fonction permet de vérifier que le jeu courant est terminé
+//SI c'est le cas contact LinkParty pour lancer le nouveau jeu
+function reloadGame(){
+	//alert("je suis reload le casse couille");
+	var finished = document.getElementById('finishGame').value;
+
+	if(finished == "true"){
+
+		xhr = getXhr();
+		clearInterval(idReloadGame);
+		document.getElementById('finishGame').value = "false";
+		changeGame();
+	}
+}
+
+function changeGame(){
+	//récupération du nom du jeu suivant + idgamesuivan
+	
+	xhr.open("POST", "./LinkParty", true);
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4 && xhr.status == 200){
+	        response = xhr.responseText;
+			
+			var servletNextGame = response;
+			alert("ResponseLinkParty : "+ response);
+			//La partie est fini
+			if(response =="0"){
+				alert(fini);
+				return;
+			}
+			servletNextGame="FindTheDot";
+			
+			//on demande à la servlet concerncé de nous envoyé les paramètres
+			servletName = servletNextGame;
+			xhr = getXhr();
+			
+			xhr.open("POST", "./" + servletName, true);
+			xhr.onreadystatechange = function(){
+				
+		        if(xhr.readyState == 4 && xhr.status == 200) {
+		            response = xhr.responseText;
+		            document.getElementById('infos').innerHTML = response;
+		            var param = response.split(';');
+		            var stringParam="";
+		            
+		          //On créer la liste des paramètre sous forme de string
+					for(var i=0;i<param.length -1;i++){
+						stringParam += param[i]+",";
+					}
+					stringParam += param[param.length-1];
+					
+					//On lance la fonction concerné
+		            eval("run"+servletNextGame+"(" + stringParam + ")");
+		            idReloadGame = setInterval("reloadGame()", 800);
+		       }
+			document.getElementById("infos");
+			};
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.send("action=1");
+		}
+		
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("idParty="+idParty);
+}
+
+
+
+
+
+/** Fonctions pour les scores de la partie **/
+
+function initScoreParty(idParty){
+	setInterval("scoreParty("+idParty+")", 500);
+}
+
+function scoreParty(idParty){
+	var servletName = "ScoreParty";
+	xhr = getXhr();
+	xhr.open("POST", "./" + servletName, true);
+	xhr.onreadystatechange = function(){
+		
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            response = xhr.responseText;
+            document.getElementById('scoreparty').innerHTML = response;
+       }
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("idParty="+idParty);
+}
+
+
+
+
+
+/** Fonctions pour le chat **/
+
+function initChat(idParty){
+	setInterval("getAllMessage("+idParty+")", 100);
+}
+
+function getAllMessage(idParty){
+	var servletName = "Chat";
+	xhr = getXhr();
+	xhr.open("POST", "./" + servletName, true);
+	xhr.onreadystatechange = function(){
+		
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            response = xhr.responseText;
+            document.getElementById('chat').innerHTML = response;
+       }
+	};
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("action=1&idParty="+idParty);
+}
+
+function sendMessage(idParty,idUser){
+	var servletName = "Chat";
+	xhr = getXhr();
+	xhr.open("POST", "./" + servletName, true);
+	xhr.onreadystatechange = function(){
+		
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            response = xhr.responseText;
+            document.getElementById('chat').innerHTML = response;
+       }
+	};
+	var msg = document.getElementsByName('message')[0].value;
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.send("action=2&idParty="+idParty+"&idUser="+idUser+"&message="+msg);
 }
