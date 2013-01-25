c87fe498ca9a1129271d988d62a67dd76b17a0b7
pierremartin799@gmail.com <pierremartin799@gmail.com@523d572e-780b-ebde-f4e8-6133ff7c8053>
2013-01-25 00:46:32 +0000

    Commit de fou après plus de 30h de PAD

--- trunk/MarilleauWare_ejb/ejbModule/controllerEJB/PartyController2Local.java
+++ trunk/MarilleauWare_ejb/ejbModule/controllerEJB/PartyController2Local.java
@@ -13,34 +13,19 @@ import model.User;
 public interface PartyController2Local {
 
 	public List<Party> getListOfParty();
-
 	public void addUser(int idParty, int idUser);
-
 	public Party getPartyById(int idParty);
-
 	public void initCurrentGame(int idParty);
-	
 	public int getIdPartyByIdUser(int idUser);
-	
 	public int getIdGameByIdParty(int idParty);
-	
 	public List<User> getAllUserPerParty(int idParty);
-	
-	//fonction chatParty
 	public void addMessage(int idParty,int idUser,String message);
-	
 	public List<ChatParty> getAllMessages(int idParty);
-	
 	public List<ChatParty> getAllMessagesLimit(int idParty,int limit);
-	
 	public List<ChatParty> getMessagesAfter(int Party,int idMessage);
-
 	public TreeMap<String, Integer> getAllScore(int idParty);
-
 	public void incrementCurrentGame(int idParty);
-
 	public void addScore(int idParty, int idPlayer, int score);
-
 	public boolean isStarted(int idParty);
 	
 	public void deletePartyAndCo(int idParty);
--- trunk/MarilleauWare_web/src/linkGame/LinkParty.java
+++ trunk/MarilleauWare_web/src/linkGame/LinkParty.java
@@ -20,31 +20,29 @@ import controllerEJB.PartyController2Local;
  */
 @WebServlet("/LinkParty")
 public class LinkParty extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-	int idPlayer;
-	int idParty;
+	private PrintWriter out;
 	
 	@EJB
 	PartyController2Local pc;
-
 	@EJB
 	GameController2Local gc;
 
-	private PrintWriter out;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public LinkParty() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		out = response.getWriter();
-		out.print("Prout 2 fois");
+		out.print("LinkParty:get");
 		out.close();
 	}
 
@@ -52,18 +50,21 @@ public class LinkParty extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		out = response.getWriter();
 		
+		response.setContentType("text/plain");
+        response.setHeader("Cache-Control", "no-cache");
+		out = response.getWriter();		
 		
 		//récupération de l'id du game suivant
 		int idParty = Integer.parseInt(request.getParameter("idParty"));
 		
 		//check start party
-				boolean start = pc.isStarted(idParty);
+		boolean start = pc.isStarted(idParty);
 						
-			if(!start){
-				out.print("-2");
-			}else{
+		if(!start){
+			out.print("-2");
+		}else{
+			
 				int idGameSuiv = gc.getNextgame(idParty);
 				System.out.println("LinkParty:  idParty: "+idParty+ " ||idGameSuiv: "+ idGameSuiv);
 				String servletName;
@@ -80,9 +81,9 @@ public class LinkParty extends HttpServlet {
 
 				}
 				out.print(""+servletName);
-			}
+
+		}
 		
 		out.close();
 	}
-
 }
